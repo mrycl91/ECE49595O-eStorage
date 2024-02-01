c177b0dff29ff534322716f6a8deef9eaa8fb323
mrycl91 <112977221+mrycl91@users.noreply.github.com>
2024-01-31 20:52:58 -0500

    Add files via upload

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by YuChen on 2024/1/16.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
