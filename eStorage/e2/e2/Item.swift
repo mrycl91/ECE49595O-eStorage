6c192fb483883d4dff0c296bf3d507660d99eaeb
lee4459 <112975998+lee4459@users.noreply.github.com>
2024-02-17 19:35:26 -0500

    Update Item.swift

--- eStorage/e2/e2/Item.swift
+++ eStorage/e2/e2/Item.swift
@@ -1,11 +1,6 @@
-// Item.swift
-// e2
-// Created by 李京樺 on 2024/1/28.
-
 import Foundation
-import SwiftData
 
-class Item: Identifiable, ObservableObject, Equatable {
+class Item: Identifiable, ObservableObject, Equatable, Codable {
     static func == (lhs: Item, rhs: Item) -> Bool {
         return lhs.id == rhs.id
     }
@@ -22,7 +17,36 @@ class Item: Identifiable, ObservableObject, Equatable {
         self.expirationDate = expirationDate
         self.notificationTime = expirationDate?.midnight()
         self.isNotificationEnabled = false
-        self.priordate=0
+        self.priordate = 0
+    }
+
+    // MARK: - Codable
+    enum CodingKeys: String, CodingKey {
+        case id, name, expirationDate, notificationTime, isNotificationEnabled, priordate
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        name = try container.decode(String.self, forKey: .name)
+        expirationDate = try container.decodeIfPresent(Date.self, forKey: .expirationDate)
+        notificationTime = try container.decodeIfPresent(Date.self, forKey: .notificationTime)
+        isNotificationEnabled = try container.decode(Bool.self, forKey: .isNotificationEnabled)
+        priordate = try container.decode(Int.self, forKey: .priordate)
+
+        // id is optional because it's generated during initialization
+        id = try container.decodeIfPresent(UUID.self, forKey: .id) ?? UUID()
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(expirationDate, forKey: .expirationDate)
+        try container.encode(notificationTime, forKey: .notificationTime)
+        try container.encode(isNotificationEnabled, forKey: .isNotificationEnabled)
+        try container.encode(priordate, forKey: .priordate)
     }
 }
 
