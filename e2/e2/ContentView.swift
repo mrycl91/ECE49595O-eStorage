74834ce85c0439e6016ba10ea1472086e11a16cf
nitaccc <156105590+nitaccc@users.noreply.github.com>
2024-03-26 19:44:12 -0400

    Update ContentView.swift
    new ui ver2

--- e2/e2/ContentView.swift
+++ e2/e2/ContentView.swift
@@ -4,36 +4,57 @@ import SwiftUI
 import Combine
 
 
-enum MealType: String, CaseIterable {
-    case breakfast = "Breakfast"
-    case lunch = "Lunch"
-    case dinner = "Dinner"
-    case somethingSimple = "Something Simple"
-}
-
-
 struct ContentView: View {
     @Binding var items: [Item]
     @State private var selectedItem: Item?
     @State private var showingDeleteAlert = false
     @State private var navigationTag: Int?
-    @State private var showingOptions = false
-    @State private var selectedMealType: MealType?
-    @State private var recommendationContent: String?
 
     // Key for UserDefaults
     private let itemsKey = "StoredItemsKey"
 
     var body: some View {
-        ZStack{
-            if showingOptions
-            {
-                MealTypeSelectionView(selectedMealType: $selectedMealType, recommendationContent: $recommendationContent) { mealType in
-                    recommendRecipes(for: mealType)
+        VStack{
+            GeometryReader { proxy in
+                Text("Grocery Lists")
+                    .font(.system(size: 26, weight: .bold))
+                    .position(x: 120, y: 60)
+                Path { path in
+                    path.move(to: CGPoint(x:proxy.size.width, y:0))
+                    path.addArc(
+                        center: CGPoint(x:proxy.size.width, y:0),
+                        radius: 110,
+                        startAngle: Angle(degrees: 90),
+                        endAngle: Angle(degrees: 180),
+                        clockwise: false
+                    )
+                    path.closeSubpath()
                 }
-            }
-            else{
+                    .fill(Color.gray)
                 
+                Button(action: {
+                    showingDeleteAlert = true
+                }, label: {
+                    Text("Delete\n       All")
+                        .foregroundColor(Color.white)
+                })
+                    .frame(width: 100, height:80)
+//                    .background(Color.blue)
+                    .position(x: proxy.size.width - 55, y: 45)
+                    .alert(isPresented: $showingDeleteAlert) {
+                        Alert(
+                            title: Text("Delete Expired Item"), message: Text("Confirm deletion of all expired items?"),
+                            primaryButton: .destructive(Text("Delete")) {
+                                deleteAllExpiredItems()
+                                showingDeleteAlert = false
+                            },
+                            secondaryButton: .cancel(){
+                                showingDeleteAlert = false
+                            }
+                        )
+                    }
+            }
+//            ScrollView{
                 VStack{
                     List {
                         ForEach(items.indices, id: \.self) { index in
@@ -59,28 +80,6 @@ struct ContentView: View {
                         .onDelete(perform: deleteItems)
                     }
                     .listStyle(PlainListStyle()) // Use PlainListStyle for a cleaner appearance
-                    
-                    VStack(spacing: 20) { // Add spacing between buttons
-                        Button(action: {
-                            deleteAllExpiredItems()
-                        }) {
-                            Text("Delete Expired Items")
-                                .padding()
-                                .foregroundColor(Color.white)
-                                .background(Color.red)
-                                .cornerRadius(8)
-                        }
-                        
-                        Button(action: {
-                            showingOptions = true
-                        }) {
-                            Text("Recommend Recipe")
-                                .padding()
-                                .background(Color.green)
-                                .foregroundColor(Color.white)
-                                .cornerRadius(8)
-                        }
-                    }
                     .alert(isPresented: $showingDeleteAlert) {
                         Alert(
                             title: Text("Delete Item"), message: Text("Are you sure you want to delete \(selectedItem?.name ?? "this item")?"),
@@ -91,76 +90,9 @@ struct ContentView: View {
                         )
                     }
                 }
-            }
+//            }
         }
     }
-
-    private func recommendRecipes(for mealType: MealType) {
-        print("Recommendation requested for meal type: \(mealType.rawValue)")
-
-        guard let selectedMealType = selectedMealType else {
-            print("Selected meal type is nil")
-            return
-        }
-        let ingredients = items.map { $0.name }.joined(separator: ", ")
-        let prompt = "What can I have for \(selectedMealType.rawValue) with \(ingredients) in my storage? Just simply list out the name,ingrediants,and steps to make it,be as concise as possible,just show 1 recipe"
-        let requestData: [String: Any] = [
-            "model": "gpt-3.5-turbo",
-            "messages": [
-                [
-                    "role": "user",
-                    "content": prompt
-                ]
-            ],
-            "max_tokens": 300
-        ]
-        let apiKey = "apikey"
-        let apiUrl = URL(string: "https://api.openai.com/v1/chat/completions")!
-        var request = URLRequest(url: apiUrl)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: requestData)
-        } catch {
-            print("Error serializing request data: \(error)")
-            return
-        }
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                print("Error calling GPT API: \(error)")
-                return
-            }
-            guard let data = data else {
-                print("No data received from GPT-3.5 API")
-                return
-            }
-            if let responseString = String(data: data, encoding: .utf8) {
-                  if let responseData = responseString.data(using: .utf8) {
-                      do {
-                          let json = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any]
-                          if let choices = json?["choices"] as? [[String: Any]], let firstChoice = choices.first, let message = firstChoice["message"] as? [String: Any], let content = message["content"] as? String {
-                              recommendationContent = content
-                          }
-                      } catch {
-                          print("Error decoding response from GPT-3.5 API: \(error)")
-                      }
-                  }
-              } else {
-                  print("Unable to decode response from GPT-3.5 API")
-              }
-          }.resume()
-      }
-
-
-//    private func addItem(_ newItem: Item) {
-//        if let index = items.firstIndex(where: { $0.expirationDate ?? Date() > newItem.expirationDate ?? Date() }) {
-//            items.insert(newItem, at: index)
-//        } else {
-//            items.append(newItem)
-//        }
-//    }
 
     private func deleteItems(at offsets: IndexSet) {
         selectedItem = items[offsets.first ?? 0]
@@ -225,72 +157,6 @@ struct ContentView: View {
     }
 }
 
-
-struct MealTypeSelectionView: View {
-    @Binding var selectedMealType: MealType?
-    @Binding var recommendationContent: String?
-    var onSelection: ((MealType) -> Void)
-    @State private var isLoading = false
-
-    var body: some View {
-        VStack(spacing: 20) {
-            Text("Recipe Recommendation🌮")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.blue)
-
-            ScrollView {
-                if let recommendationContent = recommendationContent {
-                    Text(recommendationContent)
-                        .padding()
-                        .multilineTextAlignment(.leading)
-                } else if isLoading {
-                    ProgressView("Loading Recipes...")
-                        .padding()
-                        .foregroundColor(.blue)
-                        .progressViewStyle(CircularProgressViewStyle(tint: .blue))
-                }
-            }
-            .padding()
-
-            Text("Select Meal Type🥐")
-                .font(.title)
-                .fontWeight(.semibold)
-
-            VStack(spacing: 10) {
-                ForEach(MealType.allCases, id: \.self) { type in
-                    Button(action: {
-                        selectedMealType = type
-                        isLoading = true
-                        recommendationContent = nil
-                        onSelection(type)
-                    }) {
-                        Text(type.rawValue)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                    }
-                }
-            }
-            .padding(.horizontal)
-
-            Spacer()
-        }
-        .padding()
-        .navigationBarTitle("Meal Type", displayMode: .inline)
-    }
-}
-
-
-//struct ContentView_Previews: PreviewProvider {
-//    let items: [Item] = []
-//    static var previews: some View {
-//        ContentView(items: .constent(items))
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
