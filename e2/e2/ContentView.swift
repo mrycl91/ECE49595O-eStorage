4c239d863184cf275367fcb624a2318fe3c2ab36
nitaccc <156105590+nitaccc@users.noreply.github.com>
2024-03-27 20:31:59 -0400

    Update ContentView.swift
    ContentView UI done.

--- e2/e2/ContentView.swift
+++ e2/e2/ContentView.swift
@@ -10,6 +10,7 @@ struct ContentView: View {
     @State private var showingDeleteAlert = false
     @State private var showingALLDeleteAlert = false
     @State private var navigationTag: Int?
+    @State private var detailPage: Item?
 
     // Key for UserDefaults
     private let itemsKey = "StoredItemsKey"
@@ -17,82 +18,92 @@ struct ContentView: View {
     var body: some View {
         VStack{
             GeometryReader { proxy in
-                Text("Grocery Lists")
-                    .font(.system(size: 26, weight: .bold))
-                    .position(x: 120, y: 60)
-                    .foregroundColor(Color(hex: 0x535657))
-                Path { path in
-                    path.move(to: CGPoint(x:proxy.size.width, y:0))
-                    path.addArc(
-                        center: CGPoint(x:proxy.size.width, y:0),
-                        radius: 110,
-                        startAngle: Angle(degrees: 90),
-                        endAngle: Angle(degrees: 180),
-                        clockwise: false
-                    )
-                    path.closeSubpath()
-                }
-                    .fill(Color(hex: 0x535657))
-                
-                Button(action: {
-                    showingALLDeleteAlert = true
-                }, label: {
-                    Text("Delete\n       All")
-                        .foregroundColor(Color(hex: 0xf4faff))
-                })
-                    .frame(width: 100, height:80)
-                    .position(x: proxy.size.width - 55, y: 45)
-                    .alert(isPresented: $showingALLDeleteAlert) {
-                        Alert(
-                            title: Text("Delete Expired Item"), message: Text("Confirm deletion of all expired items?"),
-                            primaryButton: .destructive(Text("Delete")) {
-                                deleteAllExpiredItems()
-                                showingALLDeleteAlert = false
-                            },
-                            secondaryButton: .cancel(){
-                                showingALLDeleteAlert = false
-                            }
-                        )
-                    }
-            }
-//            ScrollView{
                 VStack{
+                    HStack{
+                        Text("Grocery Lists")
+                            .font(.system(size:26, weight: .bold))
+                            .position(x: 110, y: 60)
+                            .foregroundColor(Color(hex: 0x535657))
+                        ZStack(alignment: .topTrailing){
+                            Path { path in
+                                path.move(to: CGPoint(x:proxy.size.width/2, y:0))
+                                path.addArc(
+                                    center: CGPoint(x:proxy.size.width/2, y:0),
+                                    radius: 110,
+                                    startAngle: Angle(degrees: 90),
+                                    endAngle: Angle(degrees: 180),
+                                    clockwise: false
+                                )
+                                path.closeSubpath()
+                            }
+                                .fill(Color(hex: 0x535657))
+                            
+                            Button(action: {
+                                showingALLDeleteAlert = true
+                            }, label: {
+                                Text("Delete\n       All")
+                                    .foregroundColor(Color(hex: 0xf4faff))
+                                    .padding(23)
+                                    .alignmentGuide(.top){ dimension in
+                                        dimension[.top]
+                                    }
+                            })
+                                .alert(isPresented: $showingALLDeleteAlert) {
+                                    Alert(
+                                        title: Text("Delete Expired Item"), message: Text("Confirm deletion of all expired items?"),
+                                        primaryButton: .destructive(Text("Delete")) {
+                                            deleteAllExpiredItems()
+                                            showingALLDeleteAlert = false
+                                        },
+                                        secondaryButton: .cancel(){
+                                            showingALLDeleteAlert = false
+                                        }
+                                    )
+                                }
+                        }
+                    }
+                    .frame(height: proxy.size.height/6)
+                    
                     List {
                         ForEach(items.indices, id: \.self) { index in
-                            NavigationLink(destination: ItemDetailView(item: items[index]), tag: index, selection: $navigationTag) {
-                                HStack {
-                                    VStack(alignment: .leading) {
-                                        Text(" \(items[index].name)")
-                                            .foregroundColor(isItemExpired(items[index].expirationDate) ? .red : .primary)
-                                        if let expirationDate = items[index].expirationDate {
-                                            Text("Expiration Date: \(formattedDate(expirationDate))")
-                                                .foregroundColor(isItemExpired(expirationDate) ? .red : .secondary)
-                                        }
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(" \(items[index].name)")
+                                        .foregroundColor(isItemExpired(items[index].expirationDate) ? .red : Color(hex: 0x535657))
+                                    if let expirationDate = items[index].expirationDate {
+                                        Text("  Best by \(formattedDate(expirationDate))")
+                                            .foregroundColor(isItemExpired(expirationDate) ? .red : Color(hex: 0x535657))
                                     }
-                                    Spacer()
                                 }
+                                Spacer()
                             }
-                            .contentShape(Rectangle())
-                            .onTapGesture {
-                                selectedItem = items[index]
-                                navigationTag = index
-                            }
+                                .contentShape(Rectangle())
+                                .onTapGesture {
+                                    selectedItem = items[index]
+                                }
+                                .sheet(item: $selectedItem){ selectedItem in
+                                    ItemDetailView(item: selectedItem)
+                                }
                         }
-                        .onDelete(perform: deleteItems)
+                            .onDelete(perform: deleteItems)
+                            .listRowBackground(Color(hex: 0xdee7e7))
                     }
-                    .listRowBackground(Color(hex: 0xdee7e7))
-                    .listStyle(PlainListStyle()) // Use PlainListStyle for a cleaner appearance
-                    .alert(isPresented: $showingDeleteAlert) {
-                        Alert(
-                            title: Text("Delete Item"), message: Text("Are you sure you want to delete \(selectedItem?.name ?? "this item")?"),
-                            primaryButton: .destructive(Text("Delete")) {
-                                deleteSelectedItem()
-                            },
-                            secondaryButton: .cancel()
-                        )
-                    }
+                        .listStyle(.plain)
+                        .alert(isPresented: $showingDeleteAlert) {
+                            Alert(
+                                title: Text("Delete Item"), message: Text("Are you sure you want to delete \(selectedItem?.name ?? "this item")?"),
+                                primaryButton: .destructive(Text("Delete")) {
+                                    deleteSelectedItem()
+                                },
+                                secondaryButton: .cancel()
+                            )
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
-//            }
+                .alignmentGuide(.bottom){ dimension in
+                    dimension[.bottom]
+                }
+            }
         }
         .background(Color(hex: 0xdee7e7))
     }
@@ -148,11 +159,9 @@ struct ContentView: View {
 }
 
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        let items: [Item] = [] // Provide sample data
-        
-        // Use a dummy Binding to pass the items array to the preview
-        ContentView(items: .constant(items))
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        let items: [Item] = []
+//        ContentView(items: .constant(items))
+//    }
+//}
