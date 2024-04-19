25801043896018d18b01bd6ad7beb8ffef8ac7b2
lee4459 <112975998+lee4459@users.noreply.github.com>
2024-04-19 18:16:02 -0400

    Update MainView.swift

--- e2/e2/MainView.swift
+++ e2/e2/MainView.swift
@@ -7,6 +7,7 @@
 import SwiftUI
 import Foundation
 
+
 enum MealType: String, CaseIterable {
     case breakfast = "Breakfast"
     case lunch = "Lunch"
@@ -37,6 +38,7 @@ struct MainView: View {
                 MealTypeSelectionView(selectedMealType: $selectedMealType, recommendationContent: $recommendationContent) { mealType in
                     recommendRecipes(for: mealType)
                 }
+                
                     .tabItem {
                         Label("Recipe", systemImage: "book.pages.fill")
                     }
@@ -82,7 +84,8 @@ struct MainView: View {
             return
         }
         let ingredients = items.map { $0.name }.joined(separator: ", ")
-        let prompt = "What can I have for \(selectedMealType.rawValue) with \(ingredients) in my storage? Just simply list out the name,ingrediants,and steps to make it,be as concise as possible,just show 1 recipe"
+        let prompt = "What can I have for \(selectedMealType.rawValue) with \(ingredients) in my storage? Just simply list out the name, ingredients, and steps to make it, be as concise as possible, just show 1 recipe. The format should be as below, the first line should just be the name of the recipe (no additional symbols), and the next section should start with the name \"Ingredients:\" and the last section should start with the name \"Steps:\""
+
         let requestData: [String: Any] = [
             "model": "gpt-3.5-turbo",
             "messages": [
@@ -94,6 +97,7 @@ struct MainView: View {
             "max_tokens": 150
         ]
         let apiKey = ProcessInfo.processInfo.environment["GPT_APIKEY"] ?? ""
+
         let apiUrl = URL(string: "https://api.openai.com/v1/chat/completions")!
         var request = URLRequest(url: apiUrl)
         request.httpMethod = "POST"
@@ -121,6 +125,7 @@ struct MainView: View {
                           let json = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any]
                           if let choices = json?["choices"] as? [[String: Any]], let firstChoice = choices.first, let message = firstChoice["message"] as? [String: Any], let content = message["content"] as? String {
                               recommendationContent = content
+//                              print(recommendationContent)
                           }
                       } catch {
                           print("Error decoding response from GPT-3.5 API: \(error)")
@@ -132,7 +137,35 @@ struct MainView: View {
           }.resume()
       }
 }
+class Recipe: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var information: String
+    
+    init(name: String, information: String) {
+        self.name = name
+        self.information = information
+    }
+}
 
+func createRecipe(from string: String) -> Recipe? {
+    // Split the string by newline characters
+    let components = string.components(separatedBy: "\n")
+    
+    // Ensure that the string contains all necessary components
+    guard components.count >= 7 else {
+        print("Invalid recipe string format")
+        return nil
+    }
+    
+    // Extract the name and information from the components
+    let name = components[0].replacingOccurrences(of: ":", with: "")
+    let information = components[1..<components.count].joined(separator: "\n")
+    
+    // Initialize a Recipe object with the extracted information
+    let recipe = Recipe(name: name, information: information)
+    return recipe
+}
 
 struct MealTypeSelectionView: View {
     @Binding var selectedMealType: MealType?
@@ -140,32 +173,69 @@ struct MealTypeSelectionView: View {
     var onSelection: ((MealType) -> Void)
     @State private var isLoading = false
     @State private var decisionMade = false
+    @State private var showingFavoriteRecipes = false
+    @State private var showAlert = false
+
+    
+    // Define a list to store favorite recipes
+    @State private var favoriteRecipes: [Recipe] = []
 
     var body: some View {
-        VStack() {
+        VStack {
             Text("")
                 .frame(maxWidth: .infinity)
             
             if decisionMade {
                 if let recommendationContent = recommendationContent {
-                    ScrollView{
-                        Text(recommendationContent)
+                    ScrollView {
+                        VStack {
+                            Text(recommendationContent)
+                                .padding()
+                                .foregroundColor(Color(hex: 0xdee7e7))
+                                .multilineTextAlignment(.leading)
+                            
+                            Button(action: {
+                                // Create a Recipe object from the recommendation content string
+                                if let recipe = createRecipe(from: recommendationContent) {
+                                    print("Recipe created:", recipe) // Debugging statement
+                                    
+                                    // Check if the recipe is already saved
+                                    if favoriteRecipes.contains(where: { $0.name == recipe.name })  {
+                                                showAlert = true
+                                                print("Recipe with name \(recipe.name) already exists in favorites") // Debugging statement
+                                            } else {
+                                                // Save the recipe as a favorite if it's not already saved
+                                                favoriteRecipes.append(recipe)
+                                                if let encoded = try? JSONEncoder().encode(self.favoriteRecipes) {
+                                                    UserDefaults.standard.set(encoded, forKey: "favoriteRecipes")
+                                                }
+                                                showAlert = true
+                                                print("Recipe with name \(recipe.name) added to favorites") // Debugging statement
+                                            }
+                                } else {
+                                    print("Failed to create Recipe object from recommendationContent")
+                                }
+                            }) {
+                                Image(systemName: "heart")
+                                    .foregroundColor(.red)
+                            }
                             .padding()
-                            .foregroundColor(Color(hex: 0xdee7e7))
-                            .multilineTextAlignment(.leading)
-                        Button("Generate New Recipe", action: {
-                            isLoading = false
-                            decisionMade = false
-                        })
-                            .frame(width: 200, height: 45)
-                            .foregroundColor(Color(hex: 0xdee7e7))
-                            .background(Color(hex: 0x89a9a9))
-                            .cornerRadius(10)
-                            .padding(30)
-                        Text("\n")
+                            .alert(isPresented: $showAlert) {
+                                       Alert(title: Text("Recipe Saved"), message: Text("The recipe has been saved to your favorites!"), dismissButton: .default(Text("OK")))
+                                   }
+                            Button("Close recipe or Generate New Recipe", action: {
+                                                            isLoading = false
+                                                            decisionMade = false
+                                                        })
+                                                        .frame(width: 200, height: 45)
+                                                        .foregroundColor(Color(hex: 0xdee7e7))
+                                                        .background(Color(hex: 0x89a9a9))
+                                                        .cornerRadius(10)
+                                                        .padding(30)
+                        }
                     }
                 } else if isLoading {
-                    VStack{
+                    VStack {
                         Spacer()
                         ProgressView("Loading Recipes...")
                             .padding()
@@ -185,15 +255,14 @@ struct MealTypeSelectionView: View {
                                         cornerRadius: 10,
                                         style: .continuous
                                     )
-                                        .fill(Color(hex: 0x89a9a9))
-                                        .stroke(Color(hex: 0x89a9a9), lineWidth: 2)
+                                    .fill(Color(hex: 0x89a9a9))
+                                    .stroke(Color(hex: 0x89a9a9), lineWidth: 2)
                                 )
                         }
                         Spacer()
                     }
                 }
-            }
-            else{
+            } else {
                 Spacer()
                 
                 Text("Time for...\n")
@@ -203,21 +272,21 @@ struct MealTypeSelectionView: View {
                     .foregroundColor(Color(hex: 0x535657))
                 
                 VStack(spacing: 10) {
-                    HStack{
+                    HStack {
                         Button(action: {
                             selectedMealType = MealType(rawValue: "Breakfast")
                             isLoading = true
                             recommendationContent = nil
-                            if let selectedMealType = selectedMealType{
+                            if let selectedMealType = selectedMealType {
                                 onSelection(selectedMealType)
                             }
                             decisionMade = true
                         }) {
-                            VStack{
+                            VStack {
                                 Image("breakfast")
                                     .resizable()
                                     .frame(width: 50, height: 50)
-                                    
+                                
                                 Text("Breakfast")
                                     .fontWeight(.semibold)
                                     .foregroundColor(Color(hex: 0x535657))
@@ -229,16 +298,16 @@ struct MealTypeSelectionView: View {
                             selectedMealType = MealType(rawValue: "Lunch")
                             isLoading = true
                             recommendationContent = nil
-                            if let selectedMealType = selectedMealType{
+                            if let selectedMealType = selectedMealType {
                                 onSelection(selectedMealType)
                             }
                             decisionMade = true
                         }) {
-                            VStack{
+                            VStack {
                                 Image("lunch")
                                     .resizable()
                                     .frame(width: 50, height: 50)
-                                    
+                                
                                 Text("Lunch")
                                     .fontWeight(.semibold)
                                     .foregroundColor(Color(hex: 0x535657))
@@ -248,21 +317,21 @@ struct MealTypeSelectionView: View {
                         }
                     }
                     Text("\n")
-                    HStack{
+                    HStack {
                         Button(action: {
                             selectedMealType = MealType(rawValue: "Dinner")
                             isLoading = true
                             recommendationContent = nil
-                            if let selectedMealType = selectedMealType{
+                            if let selectedMealType = selectedMealType {
                                 onSelection(selectedMealType)
                             }
                             decisionMade = true
                         }) {
-                            VStack{
+                            VStack {
                                 Image("dinner")
                                     .resizable()
                                     .frame(width: 50, height: 50)
-                                    
+                                
                                 Text("Dinner")
                                     .fontWeight(.semibold)
                                     .foregroundColor(Color(hex: 0x535657))
@@ -274,16 +343,16 @@ struct MealTypeSelectionView: View {
                             selectedMealType = MealType(rawValue: "Snack")
                             isLoading = true
                             recommendationContent = nil
-                            if let selectedMealType = selectedMealType{
+                            if let selectedMealType = selectedMealType {
                                 onSelection(selectedMealType)
                             }
                             decisionMade = true
                         }) {
-                            VStack{
+                            VStack {
                                 Image("snack")
                                     .resizable()
                                     .frame(width: 50, height: 50)
-                                    
+                                
                                 Text("Snack")
                                     .fontWeight(.semibold)
                                     .foregroundColor(Color(hex: 0x535657))
@@ -296,9 +365,119 @@ struct MealTypeSelectionView: View {
                 .padding(.horizontal)
                 
                 Spacer()
+                
+                Button(action: {
+                    // Toggle showing favorite recipes
+                    showingFavoriteRecipes.toggle()
+                }) {
+                    Text("Favorite Recipes")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(hex: 0xdee7e7))
+                        .frame(width: 200, height: 45)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color(hex: 0x89a9a9))
+                        )
+                }
+                .padding(.bottom)
             }
         }
-        .background(decisionMade ? Color(hex: 0x4f646f):Color(hex: 0xdee7e7))
+        .sheet(isPresented: $showingFavoriteRecipes) {
+            FavoriteRecipesListView(favoriteRecipes: $favoriteRecipes)
+        }
+        .background(decisionMade ? Color(hex: 0x4f646f) : Color(hex: 0xdee7e7))
+    }
+}
+
+struct FavoriteRecipesListView: View {
+    @Binding var favoriteRecipes: [Recipe]
+    @State private var selectedRecipe: Recipe?
+    @State private var searchText = ""
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                  
+                    Spacer()
+                    TextField("Search", text: $searchText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                        .frame(width: 200)
+                }
+                .padding(.top, 10)
+                .onTapGesture {
+                            // Dismiss keyboard when tapped outside of text field
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        }
+                
+                List {
+                    ForEach(filteredRecipes) { recipe in
+                        Button(action: {
+                            selectedRecipe = recipe
+                        }) {
+                            Text(recipe.name)
+                        }
+                    }
+                    .onDelete(perform: deleteRecipe)
+                }
+                .navigationTitle("Favorite Recipes")
+                .sheet(item: $selectedRecipe) { recipe in
+                    RecipeDetailsView(recipe: recipe)
+                        .onDisappear {
+                            selectedRecipe = nil
+                        }
+                }
+                .onAppear {
+                          // Load favorite recipes from UserDefaults when the view appears
+                          if let data = UserDefaults.standard.data(forKey: "favoriteRecipes"),
+                             let decodedRecipes = try? JSONDecoder().decode([Recipe].self, from: data) {
+                              self.favoriteRecipes = decodedRecipes
+                          }
+                      }
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willTerminateNotification)) { _ in
+                            // Save favorite recipes to UserDefaults when the app is about to terminate
+                            if let encoded = try? JSONEncoder().encode(self.favoriteRecipes) {
+                                UserDefaults.standard.set(encoded, forKey: "favoriteRecipes")
+                            }
+                        }
+            }
+        }
+    }
+    
+    private var filteredRecipes: [Recipe] {
+        if searchText.isEmpty {
+            return favoriteRecipes
+        } else {
+            return favoriteRecipes.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    private func deleteRecipe(at offsets: IndexSet) {
+        favoriteRecipes.remove(atOffsets: offsets)
+        if let encoded = try? JSONEncoder().encode(self.favoriteRecipes) {
+            UserDefaults.standard.set(encoded, forKey: "favoriteRecipes")
+        }
+    }
+}
+
+
+
+struct RecipeDetailsView: View {
+    var recipe: Recipe
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                Text(recipe.name)
+                    .font(.title)
+           
+                Text(recipe.information)
+                    .font(.body) // Adjust the font size as needed
+            }
+            .padding()
+        }
+        .navigationTitle(recipe.name)
     }
 }
 
